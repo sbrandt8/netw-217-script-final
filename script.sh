cd963bc48bbf62da778a7be3accc8203dd1a7884
teamit <sam@teamit.com>
2017-12-11 17:08:26 -0500

    Option 4 is not complete

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 
-#!/usr/bin/env bash
+#!/bin/bash
 
 #Function
 
@@ -39,7 +39,7 @@ OPTION_5() {
 OPTION_6() {
 	read -p "Input the existing user you would like to add to the group: " USER
 	read -p "Input the group you would like the existing user to go into: " GROUP
-	sudo usermod –g $GROUP $USER
+	sudo usermod –aG $GROUP $USER
 	echo
 }
 
