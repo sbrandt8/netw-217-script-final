2d0827fe4f7635600fb6d383bb8a9ae4824027e2
teamit <sam@teamit.com>
2017-12-11 16:50:52 -0500

    Script update execpt for option 5 is incomplete.

--- script.sh
+++ script.sh
@@ -8,36 +8,57 @@ OPTION_1() {
 	cat /etc/group
 }
 
-# Add group
+#Add group
 OPTION_2() {
 	read -p "Input the group name: " GROUP
 	sudo groupadd $GROUP
 }
 
+#Deleting a Group
 OPTION_3() {
-	echo
+	read -p "Input the group name you wish to delete: " GROUP
+	sudo groupdel $GROUP
+	echo "Your group is now deleted."
 }
 
+#Removing a user from a group
 OPTION_4() {
+
 	echo
 }
 
+#Renameing a group
 OPTION_5() {
-	echo
+	read -p "Input the group you would like to rename: " GROUP
+	read -p "Input the new name of that group: " NEWGROUP
+	sudo groupmod $GROUP -n $NEWGROUP
+	echo "Your group has been updated to the new name."
 }
 
+#Adding a user to a group
 OPTION_6() {
+	read -p "Input the existing user you would like to add to the group: " USER
+	read -p "Input the group you would like the existing user to go into: " GROUP
+	sudo usermod –g $GROUP $USER
 	echo
 }
 
+#Adding a new User
 OPTION_7() {
-	echo
+	read -p "Input the name of the new user you would like to create: " USER
+	sudo  useradd $USER
+}
+
+OPTION_8() {
+	echo "You have exited the menu."
+	exit
 }
 
 
 #Menu
 
 echo
+ echo "You need to select 1-7."
 echo
 echo "		Welcome to the group tool menu!"
 echo
@@ -53,9 +74,11 @@ echo "5. Rename a group"
 echo
 echo "6. Add a user to a group"
 echo
-echo "7. Exit"
+echo "7. Adding a new user"
 echo
-echo "Please select a number 1-7 to continue:"
+echo "8. Exit"
+echo
+echo "Please select a number 1-8 to continue:"
 echo
 echo "---------------------------------"
 echo
@@ -97,6 +120,10 @@ case  $USER_INPUT in
 		echo "You selected menu 7."
 		OPTION_7
 		;;
+	8)
+		echo "You selected menu 8."
+		OPTION_8
+		;;
 	*)
-		echo "You need to select 1-7."
+		 echo "You need to select 1-8."
 esac
